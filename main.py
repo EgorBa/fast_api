80848d62488cd244da36cf353416b7bd3807876c
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-03-19 15:42:03 +0300

    init

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/logo{q}")
+@app.get("/logo/{item_id}")
 def read_q(q: Optional[str] = None):
     return {"q": q}
