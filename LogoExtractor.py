fe7833773ae79841a27dfda6758ab5ea8614d334
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-03-19 16:02:57 +0300

    init1

--- LogoExtractor.py
+++ LogoExtractor.py
@@ -19,36 +19,39 @@ def get_best_picture(all_links):
 
 
 def get_icon(url):
-    result = requests.get(url)
-    content = result.text
-    soup = BeautifulSoup(content, 'lxml')
-    quotes = soup.find_all('link', attrs={'rel': re.compile("^(shortcut icon|icon)$", re.I)})
-    all_links = []
-    for r in quotes:
-        im_url = r["href"]
-        if "https://" in im_url:
-            all_links.append(im_url)
-        else:
-            all_links.append(url + im_url)
-    return get_best_picture(all_links)
+    try:
+        result = requests.get(url)
+        content = result.text
+        soup = BeautifulSoup(content, 'lxml')
+        quotes = soup.find_all('link', attrs={'rel': re.compile("^(shortcut icon|icon)$", re.I)})
+        all_links = []
+        for r in quotes:
+            im_url = r["href"]
+            if "https://" in im_url:
+                all_links.append(im_url)
+            else:
+                all_links.append(url + im_url)
+        return get_best_picture(all_links)
+    except:
+        return ""
 
 
-# def test():
-#     urls = ['https://pythonist.ru',
-#             'https://habr.com',
-#             'https://e.mail.ru',
-#             'https://stackoverflow.com',
-#             'https://tproger.ru',
-#             'https://www.endclothing.com',
-#             'https://kmu.itmo.ru',
-#             'https://www.sber.ru']
-#     counter = 0
-#     for url in urls:
-#         im_url = get_icon(url)
-#         print(im_url)
-#         if im_url != "":
-#             p = requests.get(im_url)
-#             inp = io.BytesIO(p.content)
-#             imageFile = Image.open(inp)
-#             imageFile.save("logos/" + str(counter) + ".png")
-#         counter += 1
+def test():
+    urls = ['https://pythonist.ru',
+            'https://habr.com',
+            'https://e.mail.ru',
+            'https://stackoverflow.com',
+            'https://tproger.ru',
+            'https://www.endclothing.com',
+            'https://kmu.itmo.ru',
+            'https://www.sber.ru']
+    counter = 0
+    for url in urls:
+        im_url = get_icon(url)
+        print(im_url)
+        if im_url != "":
+            p = requests.get(im_url)
+            inp = io.BytesIO(p.content)
+            imageFile = Image.open(inp)
+            imageFile.save("logos/" + str(counter) + ".png")
+        counter += 1
