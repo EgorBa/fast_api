6e461223d33b3143adcf8296578fe464005d11ac
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-03-19 20:32:38 +0300

    init3

--- main.py
+++ main.py
@@ -38,18 +38,18 @@ def read_request(im1: Optional[str] = "", im2: Optional[str] = "", im3: Optional
     W = 720
     H = 1280
 
-    input_path = get_path("in")
-    inp = io.BytesIO(im1.encode('ISO-8859-1'))
-    imageFile = Image.open(inp)
-    imageFile.save(input_path)
-    imageFile.close()
+    # input_path = get_path("in")
+    # inp = io.BytesIO(im1.encode('ISO-8859-1'))
+    # imageFile = Image.open(inp)
+    # imageFile.save(input_path)
+    # imageFile.close()
 
     path_1 = generate_one_video(
         video_len,
         x1=0, y1=0, x2=W, y2=int(H / 3),  # text position
         x3=0, y3=int(H / 3), x4=W, y4=H,  # image position
         text=desc1,  # text
-        path_to_image=input_path,  # path to image
+        path_to_image="logos/0.png",  # path to image
         animation_type='wiggle',  # type of animation
         url='https://pythonist.ru'
     )
