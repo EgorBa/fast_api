ec984caa59d5a9220605a88b6f9d0e1e1d6b4d06
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-03-22 20:50:56 +0300

    init3

--- kek.py
+++ kek.py
@@ -1,16 +1,19 @@
 import io
+import json
 
 import requests
 
-imageFileObj = open("logos/2.png", 'rb')
+imageFileObj = open("logos/1.png", 'rb')
 imageBinaryBytes = imageFileObj.read()
 imageStream = io.BytesIO(imageBinaryBytes)
 s = imageStream.read().decode('ISO-8859-1')
 
-print(s)
-p = requests.get("https://afternoon-waters-50114.herokuapp.com/create/1?desc1=kek&im1=" + str(s))
+
+print(s.encode('ISO-8859-1'))
+p = requests.get(
+    "https://afternoon-waters-50114.herokuapp.com/create/1?desc1=kek&im1=" + json.loads('{"im1":"' + str(s) + '"}'))
 print("--------------------")
-print(p.json()['video'])
+print(p.json()['video'].encode('ISO-8859-1'))
 out_file = open("videos/2.png", "wb")
 out_file.write(p.json()['video'].encode('ISO-8859-1'))
 out_file.close()
