9e9eb020e677a9939485463350fe3c3a2ae81f0e
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-03-26 18:35:47 +0300

    init3

--- kek.py
+++ kek.py
@@ -4,20 +4,41 @@ import json
 import urllib.parse
 import requests
 
-imageFileObj = open("logos/1.png", 'rb')
+import firebase_admin
+from firebase_admin import db
+
+imageFileObj = open("logos/0.png", 'rb')
 imageBinaryBytes = imageFileObj.read()
 imageStream = io.BytesIO(imageBinaryBytes)
 s = imageStream.read().decode('ISO-8859-1')
+print(len(s))
 
+cred_object = firebase_admin.credentials.Certificate('panelcreama-firebase-adminsdk-pxxap-8daf8c0b6c.json')
+default_app = firebase_admin.initialize_app(cred_object, {
+    'databaseURL': 'https://panelcreama-default-rtdb.firebaseio.com'
+})
 
-print("https://afternoon-waters-50114.herokuapp.com/create/1?desc1=kek&im1="+urllib.parse.quote(s))
-p = requests.get(
-    "https://afternoon-waters-50114.herokuapp.com/create/1?desc1=kek&im1=" + urllib.parse.quote(s))
-print("--------------------")
-print(p.json()['video'])
-out_file = open("videos/2.png", "wb")
-out_file.write(p.json()['video'].encode('ISO-8859-1'))
-out_file.close()
+# db.reference("/").child("Books").set({
+#     "Books":
+#         {
+#             "Best_Sellers": s
+#         }
+# })
+
+print(db.reference("/").child("Books").child("Books").get("Best_Sellers")[0]['Best_Sellers'])
+
+# imageFileObj = open("logos/1.png", 'rb')
+# imageBinaryBytes = imageFileObj.read()
+# imageStream = io.BytesIO(imageBinaryBytes)
+# s = imageStream.read().decode('ISO-8859-1')
+# print(len(s))
+# p = requests.get(
+#     "https://afternoon-waters-50114.herokuapp.com/create/1?desc1=kek&im1=" + urllib.parse.quote(s))
+# print("--------------------")
+# print(p.json()['video'])
+# out_file = open("videos/3.avi", "wb")
+# out_file.write(p.json()['video'].encode('ISO-8859-1'))
+# out_file.close()
 
 # h = open("samplefile.avi", 'rb')
 # p = h.read()
--- main.py
+++ main.py
@@ -6,9 +6,46 @@ from random import randrange
 import LogoExtractor
 from VideoCreator import generate_one_video
 from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import db
+import threading
+
+cred_object = firebase_admin.credentials.Certificate('panelcreama-firebase-adminsdk-pxxap-8daf8c0b6c.json')
+default_app = firebase_admin.initialize_app(cred_object, {
+    'databaseURL': 'https://panelcreama-default-rtdb.firebaseio.com'
+})
 
 diversity = 1000000
 app = FastAPI()
+
+
+def func_launch_video(image, descr):
+    video_len = 4
+    W = 720
+    H = 1280
+
+    input_path = get_path("in")
+    inp = io.BytesIO(image.encode('ISO-8859-1'))
+    imageFile = Image.open(inp)
+    imageFile.save(input_path)
+    imageFile.close()
+
+    path_1 = generate_one_video(
+        video_len,
+        x1=0, y1=0, x2=W, y2=int(H / 3),  # text position
+        x3=0, y3=int(H / 3), x4=W, y4=H,  # image position
+        text=descr,  # text
+        path_to_image=input_path,  # path to image
+        animation_type='simple',  # type of animation
+        url='https://pythonist.ru'
+    )
+
+    imageFileObj = open(path_1, 'rb')
+    imageBinaryBytes = imageFileObj.read()
+    imageStream = io.BytesIO(imageBinaryBytes)
+    s = imageStream.read().decode('ISO-8859-1')
+
+    db.reference("/").child("videos").child(id).set(s)
 
 
 class Item(BaseModel):
@@ -41,6 +78,20 @@ def read_q(q: Optional[str] = None):
 @app.post("/item/")
 async def create_item(item: Item):
     return item
+
+
+@app.get("/generate/{item_id}")
+def read_request(id: Optional[int] = 0):
+    if id == 0:
+        return {"": ""}
+
+    image = db.reference("/").child("images").child(id).get("image")[0]['image']
+    descr = db.reference("/").child("images").child(id).get("descr")[0]['descr']
+
+    th = threading.Thread(target=func_launch_video, args=(image, descr,))
+    th.start()
+
+    return {"video": "generating"}
 
 
 @app.get("/create/{item_id}")
