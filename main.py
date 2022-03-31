7a95b6c777f98cb38a36b115dd78c829e5249087
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-03-31 20:08:56 +0300

    init4

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ def read_request(id: Optional[int] = 0, colors: Optional[str] = None, animation_
         image_file.close()
         if clear_bg:
             output_path = get_path("out")
+            image_file = Image.open(inp)
             image_file.save(output_path)
             image_file.close()
             server.process_request_by_input_output_path(input_path, output_path)
