a2777926165a72a39fe8f65192ccf96fb25d2877
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-03-31 20:14:37 +0300

    init4

--- main.py
+++ main.py
@@ -94,19 +94,8 @@ def read_request(id: Optional[int] = 0, colors: Optional[str] = None, animation_
         image_file.close()
         if clear_bg:
             output_path = get_path("out")
-            image_file = Image.open(inp)
-            image_file.save(output_path)
-            image_file.close()
             server.process_request_by_input_output_path(input_path, output_path)
-            image_file_obj = open(output_path, 'rb')
-            image_binary_bytes = image_file_obj.read()
-            image_stream = io.BytesIO(image_binary_bytes)
-            s = image_stream.read().decode('ISO-8859-1')
-
-            inp = io.BytesIO(s.encode('ISO-8859-1'))
-            image_file = Image.open(inp)
-            image_file.save(input_path)
-            image_file.close()
+            input_path = output_path
 
     path_1 = generate_one_video(
         video_len,
