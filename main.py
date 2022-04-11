61b664ad3ec551165487b554c5382947476b80df
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-04-11 18:22:26 +0300

    init4

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def read_request(item_id: int, id: Optional[int] = 0, colors: Optional[str] = No
     if item_id == 0:
         path = generate_one_video(
             video_len,
-            x1=0, y1=0, x2=W, y2=H,  # text position
+            x1=30, y1=0, x2=W-30, y2=H,  # text position
             text=descr,  # text
             animation_type=animation_type,  # type of animation
             colors=get_colors_from_str(colors),  # main colors
@@ -110,7 +110,7 @@ def read_request(item_id: int, id: Optional[int] = 0, colors: Optional[str] = No
     else:
         path = generate_one_video(
             video_len,
-            x1=0, y1=0, x2=W, y2=int(H / 3),  # text position
+            x1=30, y1=0, x2=W-30, y2=int(H / 3),  # text position
             x3=0, y3=int(H / 3), x4=W, y4=H,  # image position
             text=descr,  # text
             path_to_image=input_path,  # path to image
